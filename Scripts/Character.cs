8246c751b5bc775a2955d9e5598977a13832cf8c
Quentin Gendre <swynfel@gmail.com>
2020-02-16 17:17:47 +0900

    Added random character look

---
+++ Scripts/Character.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Character : Node2D {
+	public List<ChangableSprite>  items;
+	public override void _Ready() {
+		items = GetNode("Body").GetChildren<ChangableSprite>();
+		RandomSetup();
+	}
+	
+	public void RandomSetup() {
+		foreach (var item in items)
+		{
+			item.RandomSetup();
+		}
+	}
+}
