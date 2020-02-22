00b2fd5aa77bd27a470d447a0eb4aae69c2371cf
Quentin Gendre <swynfel@gmail.com>
2020-02-22 22:55:27 +0900

    Started better sprite customization

---
+++ Scripts/Character/ChangableSprite.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class ChangableSprite : Sprite, ICustomizablePart {
+	[Export]
+	private int firstIndex;
+	[Export]
+	private int totalIndex;
+	
+	[Signal]
+	delegate void changed();
+
+	public override void _Ready () {
+		Frame = firstIndex + key;
+	}
+
+	public int key { protected set; get; } = 0;
+
+	public void Change(int to) {
+		if (key == to) {
+			return;
+		}
+		to = key;
+		Frame = firstIndex + to;
+		EmitSignal(nameof(changed));
+	}
+
+	public virtual void RandomSetup() {
+		int new_value = General.rng.Next(totalIndex);
+		Change(new_value);
+	}
+}
--- Scripts/GameHandler.cs
+++ Scripts/GameHandler.cs
@@ -3,48 +3,48 @@ using System.Collections.Generic;
 using Godot;
 
 public class GameHandler : Node2D {
-    static public GameHandler Instance { get { return myInstance; } }
+	static public GameHandler Instance { get { return myInstance; } }
 
-    static private GameHandler myInstance;
-    static public Body myPlayer;
-    static public Dictionary<string, uint> Layer = new Dictionary<string, uint> ();
+	static private GameHandler myInstance;
+	static public Body myPlayer;
+	static public Dictionary<string, uint> Layer = new Dictionary<string, uint> ();
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready () {
-        myPlayer = GetNode<Body> ("PlayerBody");
-        myInstance = this;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready () {
+		myPlayer = GetNode<Body> ("PlayerBody");
+		myInstance = this;
 
-        // Building Layer List
-        for (int i = 1; i <= 20; i += 1) {
-            string name = (string) ProjectSettings.GetSetting ("layer_names/2d_physics/layer_" + i);
+		// Building Layer List
+		for (int i = 1; i <= 20; i += 1) {
+			string name = (string) ProjectSettings.GetSetting ("layer_names/2d_physics/layer_" + i);
 
-            if (name == "")
-                name = "layer" + i;
-            Layer[name] = (uint) 1 << i - 1;
-        }
-    }
+			if (name == "")
+				name = "layer" + i;
+			Layer[name] = (uint) 1 << i - 1;
+		}
+	}
 
-    [Remote]
-    public void SpawnPlayer (int id, Vector2 position) {
-        GD.Print ("New player entering : " + id);
-        var newPlayer = ((PackedScene) GD.Load ("res://Nodes/Bodies/PlayerBody.tscn")).Instance ().GetNode<Body> ("./");
-        newPlayer.Name = id.ToString ();
-        newPlayer.SetNetworkMaster (id);
-        newPlayer.Position = position;
-        AddChild (newPlayer);
+	[Remote]
+	public void SpawnPlayer (int id, Vector2 position) {
+		GD.Print ("New player entering : " + id);
+		var newPlayer = ((PackedScene) GD.Load ("res://Nodes/Bodies/PlayerBody.tscn")).Instance ().GetNode<Body> ("./");
+		newPlayer.Name = id.ToString ();
+		newPlayer.SetNetworkMaster (id);
+		newPlayer.Position = position;
+		AddChild (newPlayer);
 
-    }
+	}
 
-    public void ControlMyPlayer (int id) {
-        myPlayer.Name = id.ToString ();
-        myPlayer.SetNetworkMaster (id);
-    }
+	public void ControlMyPlayer (int id) {
+		myPlayer.Name = id.ToString ();
+		myPlayer.SetNetworkMaster (id);
+	}
 
-    public void RemovePlayer (int id) {
-        GetNode<Node> (id.ToString ()).QueueFree ();
-    }
+	public void RemovePlayer (int id) {
+		GetNode<Node> (id.ToString ()).QueueFree ();
+	}
 
-    public void GoToMenu () {
-        //Go TO Main Menu
-    }
+	public void GoToMenu () {
+		//Go TO Main Menu
+	}
 }
