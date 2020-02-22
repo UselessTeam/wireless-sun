e5034dab15802c0788c137ce302b3fcb315329aa
Hedi <cydouzo@gmail.com>
2020-02-22 21:05:20 +0900

    Spawner synchronize

--- Scripts/Spawner/_Spawner.cs
+++ Scripts/Spawner/_Spawner.cs
@@ -6,26 +6,40 @@ public class _Spawner : Node2D
     [Export] public float SPAWN_DELAY = 2;
     [Export] public PackedScene SpawnPrefab;
 
+
+    public bool IsMaster { get { return !Network.IsConnectionStarted || IsNetworkMaster(); } }
+    public bool IsTrueMaster { get { return Network.IsConnectionStarted && IsNetworkMaster(); } }
+
     public float Radius { get { return ((CircleShape2D)GetNode<CollisionShape2D>("Area2D/CollisionShape2D").Shape).Radius; } }
 
     float timeSinceLastSpawn;
 
     public override void _Process(float delta)
     {
-        timeSinceLastSpawn += delta;
+        if (IsMaster)
+            timeSinceLastSpawn += delta;
         if (timeSinceLastSpawn > SPAWN_DELAY)
         {
             timeSinceLastSpawn = 0;
-            SpawnOne();
+            var position = GenerateSpawnPosition();
+            if (Network.IsConnectionStarted)
+                Rpc("SpawnOne", position);
+            else
+                SpawnOne(position);
         }
     }
-    void SpawnOne()
-    {
 
-        var spawnBody = SpawnPrefab.Instance().GetNode<Body>("./");
-        GD.Print(General.rng.Next());
+    Vector2 GenerateSpawnPosition()
+    {
         double theta = General.rng.NextDouble() * 2 * Math.PI;
-        spawnBody.Position = new Vector2((float)Math.Cos(theta), (float)Math.Sin(theta)) * (float)General.rng.NextDouble() * Radius;
+        return new Vector2((float)Math.Cos(theta), (float)Math.Sin(theta)) * (float)General.rng.NextDouble() * Radius;
+
+    }
+    [PuppetSync]
+    void SpawnOne(Vector2 position)
+    {
+        var spawnBody = SpawnPrefab.Instance().GetNode<Body>("./");
+        spawnBody.Position = position;
         AddChild(spawnBody);
     }
 
