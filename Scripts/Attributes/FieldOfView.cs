f9a37afd6339104532fd70e52f08f857709ab2f1
Hedi <cydouzo@gmail.com>
2020-02-26 01:56:53 +0900

    Moved feild of view To Control

--- Scripts/Attributes/FieldOfView.cs
+++ Scripts/Attributes/FieldOfView.cs
@@ -3,31 +3,31 @@ using Godot;
 
 public class FieldOfView : Area2D {
 
-    public bool IsPlayerDetected () {
-        return GetOverlappingBodies ().Count > 0;
-    }
+	public bool IsPlayerDetected () {
+		return GetOverlappingBodies ().Count > 0;
+	}
 
-    public Body GetClosestPlayer () {
-        Body closestPlayer = null;
-        float maxDist = float.PositiveInfinity;
-        foreach (PhysicsBody2D body in GetOverlappingBodies ()) {
-            float distance = (body.GlobalPosition - GetParent<Body> ().GlobalPosition).LengthSquared ();
-            if (distance < maxDist) {
-                closestPlayer = (Body) body;
-                maxDist = distance;
-            }
-        }
-        return closestPlayer;
-    }
+	public Body GetClosestPlayer () {
+		Body closestPlayer = null;
+		float maxDist = float.PositiveInfinity;
+		foreach (PhysicsBody2D body in GetOverlappingBodies ()) {
+			float distance = (body.GlobalPosition - GetParent<_Control> ().MyBody.GlobalPosition).LengthSquared ();
+			if (distance < maxDist) {
+				closestPlayer = (Body) body;
+				maxDist = distance;
+			}
+		}
+		return closestPlayer;
+	}
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready () {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready () {
 
-    }
+	}
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //
-    //  }
+	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	//  public override void _Process(float delta)
+	//  {
+	//
+	//  }
 }
--- Scripts/Control/ShadowBallControl.cs
+++ Scripts/Control/ShadowBallControl.cs
@@ -2,24 +2,24 @@ using System;
 using Godot;
 
 public class ShadowBallControl : _Control {
-    FieldOfView myFOV {
-        get { return GetNode<FieldOfView> ("../FieldOfView"); }
-    }
+	FieldOfView myFOV {
+		get { return GetNode<FieldOfView> ("FieldOfView"); }
+	}
 
-    public override void _Ready () { }
+	public override void _Ready () { }
 
-    public bool CanSeePlayer {
-        get { return myFOV.IsPlayerDetected (); }
-    }
+	public bool CanSeePlayer {
+		get { return myFOV.IsPlayerDetected (); }
+	}
 
-    public override void _PhysicsProcess (float delta) {
-        if (IsMaster) {
-            var direction = new Vector2 (0, 0);
-            if (CanSeePlayer && CanMove) {
-                var playerBody = myFOV.GetClosestPlayer ();
-                direction = (playerBody.GlobalPosition - MyBody.GlobalPosition).Normalized ();
-                MyBody.NextMovement = direction;
-            }
-        }
-    }
+	public override void _PhysicsProcess (float delta) {
+		if (IsMaster) {
+			var direction = new Vector2 (0, 0);
+			if (CanSeePlayer && CanMove) {
+				var playerBody = myFOV.GetClosestPlayer ();
+				direction = (playerBody.GlobalPosition - MyBody.GlobalPosition).Normalized ();
+				MyBody.NextMovement = direction;
+			}
+		}
+	}
 }
