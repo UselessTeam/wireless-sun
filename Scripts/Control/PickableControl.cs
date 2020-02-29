a988e181600e6c73585507284c0be6757218a027
Quentin Gendre <swynfel@gmail.com>
2020-02-29 15:53:11 +0900

    Finished world shader

--- Scripts/Control/PickableControl.cs
+++ Scripts/Control/PickableControl.cs
@@ -2,20 +2,20 @@ using System;
 using Godot;
 
 public class PickableControl : _Control {
-    [Export] string item;
-    [Export] int amount;
+	[Export] string item;
+	[Export] int amount;
 
-    public void _OnCollisionWithPlayer (KinematicCollision2D collInfo) {
-        if (IsMaster) {
-            if (Network.IsConnectionStarted)
-                Rpc ("Gather");
-            else
-                Gather ();
-        }
-    }
+	public void _OnCollisionWithPlayer (KinematicCollision2D collInfo) {
+		if (IsMaster) {
+			if (Network.IsConnectionStarted)
+				Rpc ("Gather");
+			else
+				Gather ();
+		}
+	}
 
-    [PuppetSync] public void Gather () {
-        Global.inventory.Add (Item.Manager.GetId (item));
-        GetParent<Body> ().QueueFree ();
-    }
+	[PuppetSync] public void Gather () {
+		Global.inventory.Add (Item.Manager.GetId (item));
+		GetParent<Body> ().QueueFree ();
+	}
 }
