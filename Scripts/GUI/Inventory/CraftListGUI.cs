49ee69f3581a0971bdc672bc5cf9c79dc3b17579
Hedi <hedi@sellami.dev>
2020-09-06 15:52:57 +0900

    lessCooldown

--- Scripts/GUI/Inventory/CraftListGUI.cs
+++ Scripts/GUI/Inventory/CraftListGUI.cs
@@ -3,25 +3,25 @@ using Craft;
 using Godot;
 
 public class CraftListGUI : GUIWindow {
-    [Export] private PackedScene packedCraftGUI = (PackedScene) ResourceLoader.Load ("res://Nodes/GUI/Inventory/Craft.tscn");
+	[Export] private PackedScene packedCraftGUI = (PackedScene) ResourceLoader.Load ("res://Nodes/GUI/Inventory/Craft.tscn");
 
-    public override void Maximise () {
-        this.Show ();
-    }
-    public override void Minimise () {
-        this.Hide ();
-    }
+	public override void Maximise () {
+		this.Show ();
+	}
+	public override void Minimise () {
+		this.Hide ();
+	}
 
-    public void Display (string craftLocation) {
-        for (int i = 0; i < GetChildCount (); i++) {
-            GetChild (i).QueueFree ();
-        }
-        var crafts = Craft.Manager.GetCraftLocation (craftLocation).crafts;
-        foreach (var craft in crafts) {
-            var craftGUI = (CraftGUI) packedCraftGUI.Instance ();
-            AddChild (craftGUI);
-            craftGUI.Display (craft);
-        }
-    }
+	public void Display (string craftLocation) {
+		for (int i = 1; i < GetChildCount (); i++) {
+			GetChild (i).QueueFree ();
+		}
+		var crafts = Craft.Manager.GetCraftLocation (craftLocation).crafts;
+		foreach (var craft in crafts) {
+			var craftGUI = (CraftGUI) packedCraftGUI.Instance ();
+			AddChild (craftGUI);
+			craftGUI.Display (craft);
+		}
+	}
 
 }
